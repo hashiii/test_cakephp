045775f75f654ddd4370f708c40c8b3d9844cd63
Ryo <devway20160626@gmail.com>
2017-03-20 08:29:01 +0000

    first commit

---
+++ Controller/PostsController.php
@@ -0,0 +1,55 @@
+<?php
+
+class PostsController extends AppController{
+    //ヘルパーを使えるようにします
+    public $helpoers = array('Html','Form','Flash');
+    public $componets = array('Flash');
+    //アクションを追加
+    public function index(){
+        //set?ビューにセットするっつーことです
+        $this->set('posts',$this->Post->find('all'));
+    }
+    
+    public function view($id = null){
+        if(!$id){
+            throw new NotFoundException(__('invalid post'));
+        }
+        $post = $this->Post->findById($id);
+        if(!$post){
+            throw new NotFoundException(__('Invalid post'));
+        }
+        $this->set('post',$post);
+    }
+    
+    public function add(){
+        if($this->request->is('post')){
+            //モデルの状況をリセット
+            $this -> Post->create();
+            //requstの中にリクエストされたものが入ってる
+            if($this->Post->save($this->request->data)){
+                $this->Flash->success(__('Your post has been saved'));
+                return $this->redirect(array('action'=>index));
+            }
+            //saveじゃなかったら？
+            $this->Flash->error(__('unable to add your post'));
+        }
+    }
+    
+    public function delete ($id){
+        if ($this->request->is('get')) {
+        throw new MethodNotAllowedException();
+        }
+    
+        if ($this->Post->delete($id)) {
+            $this->Flash->success(
+                __('The post with id: %s has been deleted.', h($id))
+            );
+        } else {
+            $this->Flash->error(
+                __('The post with id: %s could not be deleted.', h($id))
+            );
+        }
+    
+        return $this->redirect(array('action' => 'index'));
+    }
+}
---
+++ View/Posts/view.ctp
@@ -0,0 +1,5 @@
+<h1><?php echo h($post['Post']['title']); ?></h1>
+
+<p>created: <?php echo $post ['Post']['created'] ?></p>
+
+<p><?php echo h($post['Post']['body']); ?></p>
